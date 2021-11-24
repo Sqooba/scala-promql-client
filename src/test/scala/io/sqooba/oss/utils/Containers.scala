7ee4e36fd52c3560b3906d64d85bf58c5da78807
ex0ns <g.tournigand@gmail.com>
2021-11-24 10:31:40 +0100

    allow nested (parameter-less) promQL-function calls in queries
    GitOrigin-RevId: c8ede4c766f7a874602777025526b720f8f180b9

--- src/test/scala/io/sqooba/oss/utils/Containers.scala
+++ src/test/scala/io/sqooba/oss/utils/Containers.scala
@@ -9,7 +9,7 @@ object Containers {
   type VictoriaMetrics = Has[GenericContainer]
 
   def victoriaMetrics(
-    imageName: String = "victoriametrics/victoria-metrics:v1.42.0"
+    imageName: String = "victoriametrics/victoria-metrics:v1.53.1"
   ): ZLayer[Blocking, Nothing, VictoriaMetrics] = {
     val port = 8428
     ZManaged.make {
